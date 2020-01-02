7b0613abc1291e68f373ca105a6d58ada2199671
hans-hagen <46297181+hans-hagen@users.noreply.github.com>
2020-01-02 21:49:09 +0600

    Add files via upload

---
+++ basics.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+func main(){
+
+fmt.Println("go"+"lang")
+fmt.Println(3.14/2.0+8)
+
+fmt.Println(true && false) 
+fmt.Println(true || false)
+fmt.Println(!true)
+
+	   }
